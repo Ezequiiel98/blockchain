6a4a3aa92c4f2daf0651182345a80c7d9b00f1e1
vivianachoque <vivianaachoque@gmail.com>
2020-03-13 14:33:22 -0300

    ordene las carpetas de componentes y empece a exportar los assets

---
+++ src/components/Login/login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import blockimg from "./assets/img/blockimg.svg";
+
+export class login extends React.Component {
+
+    constructor(props){
+    super(props);
+}
+
+render() {
+
+}
