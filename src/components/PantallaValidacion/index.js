6ba050e909c04ec1e345fbbbedd88aac06fc64b6
Brankitocruz <brankocruz15@gmail.com>
2020-03-14 20:02:26 -0300

    centre la imagen superior

--- src/components/PantallaValidacion/index.js
+++ src/components/PantallaValidacion/index.js
@@ -5,7 +5,9 @@ import LineasGrises from '../../assets/img/lineasGrises.png';
 import styles from './index.module.scss';
 
 function PantallaValidacion() {
-  return <img className={styles.imagenSuperior} src={LineasGrises} />;
-}
+  return (<img className={styles.imagenSuperior} src={LineasGrises} /> <h1 className={styles.h1} > Mateo Ruiz Guiñazu</h1>;)
 
+}
 export default PantallaValidacion;
+
+
