27afe00bb83ae07a812e0fed249014803fa59838
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-03-18 01:24:48 -0300

    agrego funcionalidad de carrousel a blocks

--- src/screens/Principal/components/Blocks/index.js
+++ src/screens/Principal/components/Blocks/index.js
@@ -4,42 +4,46 @@ import styles from './index.module.scss';
 import Block from './components/Block';
 
 function Blocks() {
-  
-//   const scroll = (e ) =>{
-//         e.target.parentNode.scrollLeft += 50;
-//     console.log(    e.target.parentNode)
-//   }
-//   const scrollA = (e ) =>{
-//     e.target.parentNode.scrollLeft -= 50;
-// console.log(    e.target.parentNode)
-// }
+  const scrollNext = e => {
+    const containerBlocks = e.target.parentNode.childNodes[2];
+    containerBlocks.scrollLeft += 50;
+  };
+
+  const scrollBack = e => {
+    const containerBlocks = e.target.parentNode.childNodes[2];
+    containerBlocks.scrollLeft -= 50;
+  };
 
   return (
-    <div className={styles.containerBlocks}>
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
+    <div className={styles.container}>
+      <button type="button" onClick={scrollNext} className={styles.next} />
+      <button type="button" onClick={scrollBack} className={styles.back} />
+      <div className={styles.containerBlocks}>
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+      </div>
     </div>
   );
 }
