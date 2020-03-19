2cd344fa614565bc37f4fbe45dc2d20086d3b887
vivianachoque <vivianaachoque@gmail.com>
2020-03-19 15:05:53 -0300

    corregi algunos error en codigo con min y mayus, también estoy estilizando componente Blockimg, ancho y alto

--- src/screens/LoginScreen/components/Login/index.js
+++ src/screens/LoginScreen/components/Login/index.js
@@ -5,12 +5,12 @@ import styles from './index.module.scss';
 function Login() {
   return (
     <div className={styles.Login}>
-      <label>NOMBRE</label>
-      <input name="username" id="username" type="text" /><label>CÓDIGO DE INGRESO</label>
+      <label className={styles.label}>NOMBRE</label>
+      <input name="username" id="username" type="text" />
+      <label className={styles.label}>CÓDIGO DE INGRESO</label>
       <input name="usercode" id="usercode" type="text" />
     </div>
   );
 }
 
 export default Login;
-
