ec9c9f992f2a672dbf5fedfdcb48711825d7b972
Brankitocruz <brankocruz15@gmail.com>
2020-03-29 02:34:52 -0300

    coloque boton violeta

---
+++ src/components/App/VioletteButton/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+function VioletButton({ text }) {
+  return (
+    <div className={styles.containerButton}>
+      <button type="button" className={styles.button}>
+        { text }
+      </button>
+    </div>
+  );
+}
+
+export default VioletButton;
