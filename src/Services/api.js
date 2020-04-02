d302a42e1a48dd8d32da469627622722589a3c5f
vivianachoque <vivianaachoque@gmail.com>
2020-04-02 16:28:45 -0300

    agregué archivos a git ignore link API

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,16 +1,16 @@
 import { create } from 'apisauce';
 
-/* ueno mira eso las variables de entorno son algo que esta en tu pc nomas 
+/* ueno mira eso las variables de entorno son algo que esta en tu pc nomas
 oki
 por cuestiones de seguridadn o las subis (?  en react se declaran asi REACT_APP_ y
-   tu nombre de variable 
+   tu nombre de variable
   estase se llama API_URL entonces vamos a declararla,
    siempre las llamas usando process.env.nombreDeLaVariable,  las declaras en un archivo ,.evnv
 */
 const API_URL = process.env.REACT_APP_API_URL;
 
 const api = create({
-  baseURL: 'http://woloxblockchain.herokuapp.com/',
+  baseURL: API_URL,
   headers: {
     Authorization: 'user_token',
     'Content-Type': 'application/json',
