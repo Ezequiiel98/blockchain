55a80831408e4579eed1da1ba95815791fde1c56
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-04-08 16:11:24 -0300

    corrijo el export de cells

--- src/components/BoardResolution/index.js
+++ src/components/BoardResolution/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import cells from './constants/cells';
+import { cells } from './constants/cells';
 import styles from './index.module.scss';
 
 function BoardResolution() {
