fd5aac1c88ca91d4a2ef0ec53cdaec129f4c24d1
Brankitocruz <brankocruz15@gmail.com>
2020-04-09 00:21:19 -0300

    agregue componete button

---
+++ src/components/PantallaValidacion/Button/CustomButton/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+import verde from '../../../../assets/img/Verde.png'
+
+function button(props) {
+    if exitoso ()
+    return URL {verde};
+}
