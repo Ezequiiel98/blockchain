13c2ab9888e485cc73337dbe4a3ff2a29dbdae88
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-04-11 03:17:35 -0300

    agrego keys al board

--- src/components/PantallaValidacion/Board/index.js
+++ src/components/PantallaValidacion/Board/index.js
@@ -5,8 +5,8 @@ import styles from './index.module.scss';
 function Board() {
   const CELL_NUMBERS = 16;
   const renderCells = () =>
-    [...Array(CELL_NUMBERS)].map(() => (
-      <div className={styles.cell}>
+    [...Array(CELL_NUMBERS)].map(keyCell => (
+      <div key={keyCell} className={styles.cell}>
         <span className={styles.number}>24</span>
       </div>
     ));
