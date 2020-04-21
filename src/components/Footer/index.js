3dadbecc3a4dd04da39a7e537134b886228650b0
vivianachoque <vivianaachoque@gmail.com>
2020-04-21 16:54:19 -0300

    exporté iconos para el footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import styles from './index.module.scss';
 import violetLines from './assets/img/violetLines.png';
+import githubIcon from './assets/img/githubIcon.png';
+import linkedinIcon from './assets/img/linkedinIcon.jpg';
 
 function Footer() {
   return (
     <footer className={styles.containerFooter}>
       <img className={styles.violetLines} src={violetLines} />
+      <img className={styles.githubIcon} src={githubIcon} />
+      <img className={styles.linkedinIcon} src={linkedinIcon} />
     </footer>
+  );
 }
 
 export default Footer;
