52ee7d8f2182448544ade1339affa28c09336bed
vivianachoque <vivianaachoque@gmail.com>
2020-04-21 03:20:48 -0300

    exporté la img de violetLines, le di estilo al fondo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,2 +1,13 @@
 import React from 'react';
-import styles from 'index.module.scss';
+
+import styles from './index.module.scss';
+import violetLines from './assets/img/violetLines.png';
+
+function Footer() {
+  return (
+    <footer className={styles.containerFooter}>
+      <img className={styles.violetLines} src={violetLines} />
+    </footer>
+}
+
+export default Footer;
