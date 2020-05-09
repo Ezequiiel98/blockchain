2641e6924a32292e1ac83bec1d180830e49207c0
vivianachoque <vivianaachoque@gmail.com>
2020-05-09 05:09:51 -0300

    falta map y llamar las props en Footer

--- src/components/Footer/components/Devs/index.js
+++ src/components/Footer/components/Devs/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import styles from './index.module.scss';
-import devs from '../constants';
+import devs from '../constants/devs';
 import githubIcon from './assets/img/githubIcon.png';
 import linkedinIcon from './assets/img/linkedinIcon.jpg';
 
@@ -9,10 +9,10 @@ function Devs() {
   return (
     <div>
       <div className={styles.containerIcons}>
-        <a 'https://linkendn.com/in/${props.dev.linkedin}' <img className={styles.linkedinIcon} src={linkedinIcon} /></a>
-        <a  'https://github.com/${props.dev.github}'<img className={styles.githubIcon} src={githubIcon} /></a>
+        <a 'https://linkendn.com/in/${props.linkedin}' <img className={styles.linkedinIcon} src={linkedinIcon} /></a>
+        <a  'https://github.com/${props.github}'<img className={styles.githubIcon} src={githubIcon} /></a>
       </div>
-      <span>Ezequiel Aragón Viviana Choque Branko Cruz</span>
+      <span> {props.name}</span>
     </div>
   );
 }
