73f63f2ad998eaf029ca0f1ccdb88b595933cb99
vivianachoque <vivianaachoque@gmail.com>
2020-05-09 01:38:14 -0300

    arregle componente footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import styles from './index.module.scss';
 import Devs from './components/Devs';
+import violetLines from './assets/img/violetLines.png';
 
 function Footer() {
   return (
     <footer className={styles.containerFooter}>
+      <img className={styles.violetLines} src={violetLines} />
       <Devs />
     </footer>
   );
