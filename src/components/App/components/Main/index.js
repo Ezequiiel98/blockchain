fe23fc8cebb8b4746e93d7059467650585af3eba
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-05-19 02:53:09 -0300

    agrego ruta por si intentan entrar a una ruta que no existe, lo redirija al login por defecto

--- src/components/App/components/Main/index.js
+++ src/components/App/components/Main/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import LoginScreen from '../../../../screens/LoginScreen';
 import Principal from '../../../../screens/Principal';
@@ -12,7 +12,8 @@ function Main() {
     <Switch>
       <Route exact path="/" component={LoginScreen} />
       <PrivateRouter exact path="/game" component={Principal} />
-      <Route exact path="/validation" component={PantallaValidacion} />
+      <PrivateRouter exact path="/validation" component={PantallaValidacion} />
+      <Route path="/" render={() => <Redirect to="/" />} />
     </Switch>
   );
 }
